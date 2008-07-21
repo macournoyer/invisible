de7385e0553bade97d2c7fcf2e9d8ee2ae693a2e
macournoyer <macournoyer@gmail.com>
2008-07-21 16:15:45 -0400

    Add some doc

--- lib/invisible.rb
+++ lib/invisible.rb
@@ -2,6 +2,40 @@ require "rubygems"
 require "thin"
 require "markaby"
 
+# = The Invisible framework class
+# If Camping was a micro-framwork at 4K then Invisible is a pico-framework of 2K.
+# Half the size mainly because of Rack. Many ideas were barrowed from Sinatra,
+# but with a few more opinions on my own and a strong emphasis on compactness.
+#
+# == Build an app in an object
+# Invisible supports multiple applications running in the same VM. Each instance
+# of this class represents a runnable application.
+#
+#  app = Invisible.new do
+#    get "/" do
+#      render "ohaie"
+#    end
+#  end
+#
+# == Build an app in a file
+# DSL like Sinatra is also supported, put all your `get`, `post`, `layout` in your
+# naked file and Invisible will do the method_missing magic for you.
+#
+# == Your app is a Rack config file (or not)
+# Often the problem with new frameworks is you have to find how to deploy it.
+# You either can make your app file standalone and runnable on its own, put this
+# at the end of your file:
+# 
+#  app.run
+# 
+# Or to use as a Rack config file, switch the 2 and remove the dot
+# 
+#  run app
+# 
+# Then you'll be able to run with Thin:
+# 
+#  thin start -R app.ru
+# 
 class Invisible
   HTTP_METHODS = [:get, :post, :head, :put, :delete]
   attr_reader :request, :params
@@ -19,6 +53,15 @@ class Invisible
   end
   HTTP_METHODS.each { |m| class_eval "def #{m}(r, &b); process('#{m}', r, &b) end" }
   
+  # Render the response.
+  # Render markaby by passing a block:
+  #
+  #  render "some text"
+  # 
+  # or simple text as the first argument.
+  # 
+  #  render "some text"
+  #
   def render(*args, &block)
     status  = args.first.is_a?(Fixnum) ? args.shift : 200
     options = args.last.is_a?(Hash) ? args.pop : {}
